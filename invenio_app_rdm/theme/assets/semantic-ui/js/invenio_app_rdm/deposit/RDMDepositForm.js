62ad864d320a4c67dd0cb4cfe32dd38c548d7b5a
Thomas Morrell <tmorrell@users.noreply.github.com>
2023-07-24 15:25:47 -0700

    Revert "Custom fields active configuration"
    This reverts commit 73ade6dab98162983b9166eddc8687b045ad7784.

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/RDMDepositForm.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/RDMDepositForm.js
@@ -11,7 +11,7 @@ import { i18next } from "@translations/invenio_app_rdm/i18next";
 import _get from "lodash/get";
 import _isEmpty from "lodash/isEmpty";
 import React, { Component, createRef, Fragment } from "react";
-import { AccordionField } from "react-invenio-forms";
+import { AccordionField, CustomFields } from "react-invenio-forms";
 import {
   AccessRightField,
   DescriptionsField,
@@ -43,93 +43,6 @@ import { FundingField } from "@js/invenio_vocabularies";
 import { Card, Container, Grid, Ref, Sticky } from "semantic-ui-react";
 import PropTypes from "prop-types";
 import Overridable from "react-overridable";
-
-// This file is part of React-Invenio-Forms
-// Copyright (C) 2022 CERN.
-// Copyright (C) 2022 Northwestern University.
-//
-// React-Invenio-Forms is free software; you can redistribute it and/or modify it
-// under the terms of the MIT License; see LICENSE file for more details.
-
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { AccordionField } from "../../AccordionField";
-import { loadWidgetsFromConfig } from "../loader";
-
-// Defined CustomFields here to set then to be closed by default
-
-export class CustomFields extends Component {
-  state = { sections: [] };
-
-  componentDidMount() {
-    const { includesPaths, fieldPathPrefix } = this.props;
-    // use of `Promise.then()` as eslint is giving an error when calling setState() directly
-    // in the componentDidMount() method
-    this.loadCustomFieldsWidgets()
-      .then((sections) => {
-        sections = sections.map((sectionCfg) => {
-          const paths = includesPaths(sectionCfg.fields, fieldPathPrefix);
-          return { ...sectionCfg, paths };
-        });
-        this.setState({ sections });
-      })
-      .catch((error) => {
-        console.error("Couldn't load custom fields widgets.", error);
-      });
-  }
-
-  async loadCustomFieldsWidgets() {
-    const { config, fieldPathPrefix, templateLoaders } = this.props;
-
-    const sections = [];
-    for (const sectionCfg of config) {
-      // Path to end user's folder defining custom fields ui widgets
-      const fields = await loadWidgetsFromConfig({
-        templateLoaders: templateLoaders,
-        fieldPathPrefix: fieldPathPrefix,
-        fields: sectionCfg.fields,
-      });
-      sections.push({ ...sectionCfg, fields });
-    }
-    return sections;
-  }
-
-  render() {
-    const { sections } = this.state;
-    return (
-      <>
-        {sections.map(({ section, fields, paths }) => (
-          <AccordionField key={section} includesPaths={paths} label={section} >
-            {fields}
-          </AccordionField>
-        ))}
-      </>
-    );
-  }
-}
-
-CustomFields.propTypes = {
-  config: PropTypes.arrayOf(
-    PropTypes.shape({
-      section: PropTypes.string.isRequired,
-      fields: PropTypes.arrayOf(
-        PropTypes.shape({
-          field: PropTypes.string.isRequired,
-          ui_widget: PropTypes.string.isRequired,
-          props: PropTypes.object,
-        })
-      ),
-    })
-  ).isRequired,
-  templateLoaders: PropTypes.array.isRequired,
-  fieldPathPrefix: PropTypes.string.isRequired,
-  includesPaths: PropTypes.func,
-};
-
-CustomFields.defaultProps = {
-  includesPaths: (fields) => fields.map((field) => field.key),
-};
-
 
 export class RDMDepositForm extends Component {
   constructor(props) {
