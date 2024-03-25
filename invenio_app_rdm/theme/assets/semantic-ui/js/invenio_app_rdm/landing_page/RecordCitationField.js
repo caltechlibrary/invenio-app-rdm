f7dcda87bc159e9661f5aa6aef1dbb849f032d46
Tom Morrell <tmorrell@caltech.edu>
2024-03-25 21:28:24 +0000

    records-ui: urlize links in citation text

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/RecordCitationField.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/RecordCitationField.js
@@ -103,7 +103,7 @@ export class RecordCitationField extends Component {
 
     // convert links in text to clickable links (ignoring punctuations at the end)
     const escapedCitation = _escape(citation); // escape html characters
-    const urlRegex = /\bhttps?:\/\/\S+?(?=[.,?!;]*\s|$)/g;
+    const urlRegex = /(https?:\/\/[^\s,;]+(?=[^\s.,;]*\S))/g;
     const urlizedCitation = escapedCitation.replace(urlRegex, (url) => {
       return `<a href="${url}" target="_blank">${url}</a>`;
     });
